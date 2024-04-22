ef168049044fe03ab21669344a5230fe2a634757
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 18:35:46 +0530

    functions concept

---
+++ Functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Let us see the functions use 
+#functions come into picture if there is repetated code 
+
+RTO=$(id -u)
+
+Validate(){
+    if ($0 -eq 0 )
+    then 
+        echo "Success"
+    else    
+        echo "Failure"
+    fi
+}
+
+if( $RTO -eq 0 )
+then 
+    echo " you are root user"
+else
+    echo "run with root user"
+    exit 1 #in shell script we need to check manually 
+fi
+
+
+dnf install mysql -y 
+Validate $? "Installing Mysql"
+
+
+
+
