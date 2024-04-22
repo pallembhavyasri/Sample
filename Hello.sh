472ad919b44911484235b07fc4bd54d1b264e3ec
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 20:19:38 +0530

    hello script with colours

--- Hello.sh
+++ Hello.sh
@@ -5,3 +5,7 @@ echo "hello world welcome to devops"
 #to print the message in colour formal 
 
 echo -e "\e[33m Hello World"
+
+#to change the colours for word wise
+
+echo -e "\e[31mHello \e[32World"
