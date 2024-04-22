b1e2ef58a3d916650edeb8eac99f9916b2d6f514
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 11:38:34 +0530

    operators concept

--- Operators.sh
+++ Operators.sh
@@ -10,3 +10,9 @@ SUM=$(($NUM1+$NUM2))
 
 echo "The sum of $NUM1 & $NUM2 is : $SUM"
 
+#If we pass the string in place of number it is exclue the value exceution 
+#eg: 1 sai --- it will give o/p as 1 
+#eg: sai pavan -- it will give the o/p as 0  
+
+
+
