16ad38594bf11925f713871b3579e5493201c97f
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 16:03:21 +0530

    special variables concept

--- Special-Variable.sh
+++ Special-Variable.sh
@@ -5,5 +5,6 @@ echo "no of variables passed : $#"
 echo "to find the script name : $0"
 echo "to get the present working directory : $PWD"
 echo "to get the home directory : $HOME"
+echo "to know which user is running : $USER"
 
 
