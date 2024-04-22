d7fa0ec2cc0c12612ccfc8007a08d846dffc77d3
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 11:18:35 +0530

    arrays datatype concept

---
+++ Array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#index starts from 0,1,...
+#to print all the elements in array we need to give te @ in the index value 
+
+#example
+
+Makeup brands = ("MAC" "ELF" "NARS" "RAREBEAUTY")
+
+echo "First Brand is : ${Makeup brands[0]}"
