941b54d48eb0cec3ff1fe2fdc69da35c2ca5fb4b
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 16:02:45 +0530

    special variables concept

--- Special-Variable.sh
+++ Special-Variable.sh
@@ -4,4 +4,6 @@ echo "print all variable : $@"
 echo "no of variables passed : $#"
 echo "to find the script name : $0"
 echo "to get the present working directory : $PWD"
+echo "to get the home directory : $HOME"
+
 
