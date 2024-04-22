0e28e553da2f250ac02c6155ad968c98f416a34b
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-22 10:57:53 +0530

    runtime variables concept

---
+++ Runtime-Variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Some confiential information we can't store in script so we give at the time exceution like username & password
+#so there is comman called read --This is visible when we enter. 
+#To hide while entering also we can give read -s 
+
+#sample example
+
+echo "Enter name of the user"
+
+read -s Username
+
+echo "Enter the password"
+
+read -s Password
