e42cfccf764b4a9064d6258fff2bcb012f51892e
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-23 12:54:49 +0530

    access of script concepts

--- Current-script.sh
+++ Current-script.sh
@@ -2,9 +2,9 @@
 
 course="content from current script"
 
-echo " Before calling the other script : $course "
-echo " Process instance ID of the current sprint : $$ " 
+echo "Before calling the other script : $course"
+echo  "Process instance ID of the current sprint : $$" 
 
-./Current-script.sh 
+./Other-script.sh 
 
-echo "After calling the other script : $course "
+echo "After calling the other script : $course"
---
+++ Other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+course="Content from other script"
+
+echo "Before calling the other script : $course"
+echo "Process ID of the script : $$"
