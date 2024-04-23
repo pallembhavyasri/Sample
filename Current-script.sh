c43a7ea18610ae1028383a0bcf5a4a4d56fb352e
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-23 13:08:16 +0530

    access of script concepts

--- Current-script.sh
+++ Current-script.sh
@@ -5,6 +5,15 @@ course="content from current script"
 echo "Before calling the other script : $course"
 echo  "Process instance ID of the current sprint : $$" 
 
+#the first way
+#This is way will not have any relation btw 2 scripts
+#The values will not change after the 2 script called hence will not have any effect on values but Process ID will be differet
 ./Other-script.sh 
+
+
+#the second way
+#This is way will  have any relation btw 2 scripts
+#The values will  change after the 2 script called hence will  have any effect on values but Process ID will be same
+source ./Other-script.sh 
 
 echo "After calling the other script : $course"
