452a52cf331f62d1c486686a950416442f926a92
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-23 13:09:03 +0530

    access of script concepts

--- Current-script.sh
+++ Current-script.sh
@@ -8,7 +8,8 @@ echo  "Process instance ID of the current sprint : $$"
 #the first way
 #This is way will not have any relation btw 2 scripts
 #The values will not change after the 2 script called hence will not have any effect on values but Process ID will be differet
-./Other-script.sh 
+
+#./Other-script.sh 
 
 
 #the second way
